b5c22cd97da555ddb80a3e3a45e2aa3fd3009f73
Cayo Hollanda <cayohollanda@hotmail.com>
2019-01-04 16:42:49 -0300

    alterando forma de buscar environments e forma de mostrá-los na view (dinamizando criação dos inputs dos environments)

--- src/services/EnvService.js
+++ src/services/EnvService.js
@@ -1,7 +1,7 @@
 import { Http } from '../utils/Http'
 
 const getEnvs = () => {
-    return Http.get("/hello")
+    return Http.get("/env")
         .then(resp => {
             return Promise.resolve(resp.data)
         })
--- src/views/Dashboard/Dashboard.jsx
+++ src/views/Dashboard/Dashboard.jsx
@@ -30,13 +30,12 @@ import { envService } from '../../services/EnvService'
 import dashboardStyle from "assets/jss/material-dashboard-react/views/dashboardStyle.jsx";
 
 class Dashboard extends React.Component {
-  statusBot = false;
+  statusBot = true;
   qntComandos = 7;
 
-  environments = envService.getEnvs()
-
   state = {
-    value: 0
+    value: 0,
+    envs: [{}]
   };
   handleChange = (event, value) => {
     this.setState({ value });
@@ -45,8 +44,42 @@ class Dashboard extends React.Component {
   handleChangeIndex = index => {
     this.setState({ value: index });
   };
+  componentDidMount() {
+    envService.getEnvs()
+      .then(response => {
+        const newEnvs = response
+
+        const newState = Object.assign({}, this.state, {
+          envs: newEnvs
+        });
+
+        this.setState(newState);
+        console.log(this.state)
+      })
+      .catch(error => console.log(error));
+  }
+  createInputs() {
+    let inputs = []
+
+    for(let i = 0; i < this.state.envs.length; i++) {
+      inputs.push(
+        <GridItem xs={12} sm={12} md={4}>
+          <CustomInput
+            labelText={this.state.envs[i].key}
+            id={this.state.envs[i].key}
+            formControlProps={{
+              fullWidth: true
+            }}
+            inputProps={{
+              value: String(this.state.envs[i].value)
+            }}
+          />
+        </GridItem>
+      )
+    }
+    return inputs
+  }
   render() {
-    console.log(this.environments)
     const { classes } = this.props;
     const wrapperDiv = classNames(
       classes.checkboxAndRadio,
@@ -71,122 +104,7 @@ class Dashboard extends React.Component {
                       <h3>Environments</h3>
                     </GridItem>
 
-                    <GridItem xs={12} sm={12} md={4}>
-                      <CustomInput
-                        labelText="RANCHER_ACCESS_KEY"
-                        id="rancherAccessKey"
-                        formControlProps={{
-                          fullWidth: true
-                        }}
-                        inputProps={{
-                          value: "ASOJDASD9A8982131LASKDJKL"
-                        }}
-                      />
-                    </GridItem>
-
-                    <GridItem xs={12} sm={12} md={4}>
-                      <CustomInput
-                        labelText="RANCHER_SECRET_KEY"
-                        id="rancherSecretKey"
-                        formControlProps={{
-                          fullWidth: true
-                        }}
-                        inputProps={{
-                          value: "ASOJDASD9A8982131LASKDJKL"
-                        }}
-                      />
-                    </GridItem>
-
-                    <GridItem xs={12} sm={12} md={4}>
-                      <CustomInput
-                        labelText="RANCHER_BASE_URL"
-                        id="rancherBaseUrl"
-                        formControlProps={{
-                          fullWidth: true
-                        }}
-                        inputProps={{
-                          value: "http://18.170.25.1:8080/v1/projects"
-                        }}
-                      />
-                    </GridItem>
-
-                    <GridItem xs={12} sm={12} md={4}>
-                      <CustomInput
-                        labelText="RANCHER_PROJECT_ID"
-                        id="rancherProjectId"
-                        formControlProps={{
-                          fullWidth: true
-                        }}
-                        inputProps={{
-                          value: "1a5"
-                        }}
-                      />
-                    </GridItem>
-
-                    <GridItem xs={12} sm={12} md={4}>
-                      <CustomInput
-                        labelText="SLACK_BOT_TOKEN"
-                        id="slackBotToken"
-                        formControlProps={{
-                          fullWidth: true
-                        }}
-                        inputProps={{
-                          value: "aosjhdoasjodias9721821asl"
-                        }}
-                      />
-                    </GridItem>
-
-                    <GridItem xs={12} sm={12} md={4}>
-                      <CustomInput
-                        labelText="SLACK_BOT_ID"
-                        id="slackBotId"
-                        formControlProps={{
-                          fullWidth: true
-                        }}
-                        inputProps={{
-                          value: "AOASD719"
-                        }}
-                      />
-                    </GridItem>
-
-                    <GridItem xs={12} sm={12} md={4}>
-                      <CustomInput
-                        labelText="SLACK_BOT_CHANNEL"
-                        id="slackBotChannel"
-                        formControlProps={{
-                          fullWidth: true
-                        }}
-                        inputProps={{
-                          value: "MHAO7A9S"
-                        }}
-                      />
-                    </GridItem>
-
-                    <GridItem xs={12} sm={12} md={4}>
-                      <CustomInput
-                        labelText="SLACK_BOT_VERIFICATION_TOKEN"
-                        id="slackBotVerificationToken"
-                        formControlProps={{
-                          fullWidth: true
-                        }}
-                        inputProps={{
-                          value: "ajsdosa899879A8SDJAIOJ989LKJASD989"
-                        }}
-                      />
-                    </GridItem>
-
-                    <GridItem xs={12} sm={12} md={4}>
-                      <CustomInput
-                        labelText="HTTP_PORT"
-                        id="httpPort"
-                        formControlProps={{
-                          fullWidth: true
-                        }}
-                        inputProps={{
-                          value: "9090"
-                        }}
-                      />
-                    </GridItem>
+                    {this.createInputs()}
                   </GridContainer>
                 </GridItem>
               </GridContainer>
